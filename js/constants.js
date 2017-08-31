f95ab48134d864014aaf15045dd8687215ea580d
andrewkolos <andrewrkolos@gmail.com>
2017-08-30 20:21:12 -0400

    add lighting, change wall color to white

--- js/constants.js
+++ js/constants.js
@@ -5,3 +5,12 @@ const NEAR_CLIPPING_PLANE = 0.1;
 const FAR_CLIPPING_PLANE = 10000;
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
+const CAMERA_X = 0;
+const CAMERA_Y = -20;
+const CAMERA_Z = 10;
+
+const PLAYFIELD_WIDTH = 10;
+const PLAYFIELD_HEIGHT = 20;
+
+const WALL_WIDTH = 0.5;
+const WALL_COLOR = 0xffffff;
--- js/initialization.js
+++ js/initialization.js
@@ -7,7 +7,7 @@ function init() {
     renderer.shadowMapEnabled = true;
 
     camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT_RATIO, NEAR_CLIPPING_PLANE, FAR_CLIPPING_PLANE);
-    camera.position.set(0, -20, 10);
+    camera.position.set(CAMERA_X, CAMERA_Y, CAMERA_Z);
     camera.lookAt(scene.position);
 
 
@@ -31,7 +31,7 @@ function init() {
 }
 
 function createPlayField() {
-    var geometry = new THREE.PlaneGeometry(10, 20);
+    var geometry = new THREE.PlaneGeometry(PLAYFIELD_WIDTH, PLAYFIELD_HEIGHT);
     var material = new THREE.MeshLambertMaterial({color: 0x156289});
     var plane = new THREE.Mesh(geometry, material);
     plane.position.set(0,0,0);
@@ -39,18 +39,24 @@ function createPlayField() {
     var lineGeometry = new THREE.PlaneGeometry(10, 0.5);
     var lineMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
     var line = new THREE.Mesh(lineGeometry, lineMaterial);
-    line.position.set(0, 0.2, 0);
+    line.position.set(0, plane.position.x + 0.2, 0);
 
     scene.add(plane);
     scene.add(line);
 }
 
 function createWalls() {
-    var leftWallGeometry = new THREE.BoxGeometry(1, 20, 1);
-    var leftWallMaterial = new THREE.MeshLambertMaterial({color: 0xd1ffde});
+    var leftWallGeometry = new THREE.BoxGeometry(WALL_WIDTH, PLAYFIELD_HEIGHT, 1);
+    var leftWallMaterial = new THREE.MeshLambertMaterial({color: WALL_COLOR});
     var leftWall = new THREE.Mesh(leftWallGeometry, leftWallMaterial);
-    leftWall.position.set(-5.5, 0, 0.5);
+    leftWall.position.set(-PLAYFIELD_WIDTH / 2 - (WALL_WIDTH/2), 0, WALL_WIDTH);
     scene.add(leftWall);
+
+    var rightWallGeometry = new THREE.BoxGeometry(WALL_WIDTH, PLAYFIELD_HEIGHT, 1);
+    var rightWallMaterial = new THREE.MeshLambertMaterial({color: WALL_COLOR});
+    var rightWall = new THREE.Mesh(rightWallGeometry, rightWallMaterial);
+    rightWall.position.set(PLAYFIELD_WIDTH / 2 + (WALL_WIDTH/2), 0, WALL_WIDTH);
+    scene.add(rightWall);
 }
 
 function createPaddles() {
@@ -71,8 +77,20 @@ function createSpotlight() {
 
     var light = new THREE.DirectionalLight(0xffffff, 2);
     light.castShadow = true; // expensive
-    light.position.set(10, 20, 20);
+    light.position.set(-5, 1, 5);
     //light.shadowCameraNear = 20;
     //light.shadowCameraFar = 50;
     scene.add(light);
+
+    var dirHelper = new THREE.DirectionalLightHelper(light);
+    scene.add(dirHelper);
+
+    var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6);
+    hemiLight.color.setHSL( 0.6, 1, 0.6 );
+    hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+    hemiLight.position.set( 0, 0, 10 );
+    scene.add( hemiLight );
+
+    var hemiHelper = new THREE.HemisphereLightHelper(hemiLight);
+    scene.add(hemiHelper);
 }
