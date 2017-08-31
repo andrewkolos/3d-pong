feeed20d34b94de2a9f69e9f2b626ad9b958c4df
andrewkolos <andrewrkolos@gmail.com>
2017-08-31 01:30:03 -0400

    fix shadows, add paddles, add ball

--- js/initialization.js
+++ js/initialization.js
@@ -8,7 +8,7 @@ function init() {
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setClearColor(0x000000, 1.0);
     renderer.setSize(WIDTH, HEIGHT);
-    renderer.shadowMapEnabled = true;
+    renderer.shadowMap.enabled= true;
     renderer.shadowMapSoft = true;
 
 
@@ -39,37 +39,35 @@ function render() {
 }
 
 function createPlayField() {
+    function createPlayPlane(color, length, offset) {
+        var geometry = new THREE.PlaneGeometry(PLAYFIELD_WIDTH, length);
+        var material = new THREE.MeshLambertMaterial({color: color});
+        var plane = new THREE.Mesh(geometry, material);
+        plane.material.side = THREE.DoubleSide;
+        plane.receiveShadow = true;
+        plane.position.set(0, offset,0);
+
+        scene.add(plane);
+    }
+
     var playPlaneLength = (PLAYFIELD_HEIGHT / 2) - (CENTERLINE_WIDTH / 2);
     var playPlanePos = (CENTERLINE_WIDTH / 2) + (playPlaneLength / 2);
-    var backPlane = createPlayPlane(PLAYFIELD_COLOR, playPlaneLength, playPlanePos);
-    var frontPlane = createPlayPlane(PLAYFIELD_COLOR, playPlaneLength, -playPlanePos);
-    var linePlane = createPlayPlane(0xFFFFFF, CENTERLINE_WIDTH, 0);
-
-    scene.add(backPlane);
-    scene.add(frontPlane);
-    scene.add(linePlane);
+    createPlayPlane(PLAYFIELD_COLOR, playPlaneLength, playPlanePos);
+    createPlayPlane(PLAYFIELD_COLOR, playPlaneLength, -playPlanePos);
+    createPlayPlane(0xFFFFFF, CENTERLINE_WIDTH, 0);
 }
 
-function createPlayPlane(color, length, offset) {
-    var geometry = new THREE.PlaneGeometry(PLAYFIELD_WIDTH, length);
-    var material = new THREE.MeshLambertMaterial({color: color});
-    var plane = new THREE.Mesh(geometry, material);
-    plane.material.side = THREE.DoubleSide;
-    plane.receiveShadow = true;
-    plane.position.set(0, offset,0);
 
-    return plane;
-}
 
 function createWalls() {
-    var leftWallGeometry = new THREE.BoxGeometry(WALL_WIDTH, PLAYFIELD_HEIGHT, 1);
+    var leftWallGeometry = new THREE.BoxGeometry(WALL_WIDTH, PLAYFIELD_HEIGHT, WALL_HEIGHT);
     var leftWallMaterial = new THREE.MeshLambertMaterial({color: WALL_COLOR});
     var leftWall = new THREE.Mesh(leftWallGeometry, leftWallMaterial);
     leftWall.position.set(-PLAYFIELD_WIDTH / 2 - (WALL_WIDTH/2), 0, WALL_WIDTH);
     leftWall.castShadow = true;
     scene.add(leftWall);
 
-    var rightWallGeometry = new THREE.BoxGeometry(WALL_WIDTH, PLAYFIELD_HEIGHT, 1);
+    var rightWallGeometry = new THREE.BoxGeometry(WALL_WIDTH, PLAYFIELD_HEIGHT, WALL_HEIGHT);
     var rightWallMaterial = new THREE.MeshLambertMaterial({color: WALL_COLOR});
     var rightWall = new THREE.Mesh(rightWallGeometry, rightWallMaterial);
     rightWall.position.set(PLAYFIELD_WIDTH / 2 + (WALL_WIDTH/2), 0, WALL_WIDTH);
@@ -78,7 +76,23 @@ function createWalls() {
 }
 
 function createPaddles() {
+    function createPaddle(color, offset) {
+        var paddleGeometry = new THREE.BoxGeometry(PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_DEPTH);
+        var paddleMaterial = new THREE.MeshLambertMaterial({ color: color });
 
+        var paddle = new THREE.Mesh(paddleGeometry, paddleMaterial);
+        paddle.position.set(0, offset, PADDLE_DEPTH / 2);
+        paddle.castShadow = true;
+        paddle.receiveShadow = true;
+
+        return paddle;
+    }
+
+    playerPaddle = createPaddle(PADDLE_PLAYER_COLOR, - (PLAYFIELD_HEIGHT / 2) + PADDLE_HEIGHT/ 2);
+    computerPaddle = createPaddle(PADDLE_COMPUTER_COLOR, PLAYFIELD_HEIGHT / 2);
+
+    scene.add(playerPaddle)
+    scene.add(computerPaddle);
 }
 
 function createScoreBoard() {
@@ -86,22 +100,29 @@ function createScoreBoard() {
 }
 
 function createBall() {
+    var geomtry = new THREE.SphereGeometry(BALL_RADIUS, BALL_NUM_SEGMENTS, BALL_NUM_SEGMENTS);
+    var material = new THREE.MeshLambertMaterial({ color: BALL_COLOR });
+    ball = new THREE.Mesh(geomtry, material);
+    ball.position.set(0,0,BALL_RADIUS);
+    ball.receiveShadow = true;
+    ball.castShadow = true;
 
+    scene.add(ball);
 }
 
 function createSpotlight() {
     // We choose directional light as it can be used to simulate sunlight.
     // See three.js documentation.
 
-    var light = new THREE.DirectionalLight(0xffffff, 1.4);
+    var light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(-7, 0, 8);
     light.castShadow = true; // expensive
     light.shadowCameraVisible = true;
-    light.shadowDarkness = 0.00;
-    light.shadowCameraNear = 6;
+    light.shadowDarkness = 1;
+    light.shadowCameraNear = 3;
     light.shadowCameraFar = camera.far;
-    light.shadow.mapSize.width = 10;
-    light.shadow.mapSize.height = 10;
+    //light.shadow.mapSize.width = 29;
+    //light.shadow.mapSize.height = 20;
     light.shadowCameraLeft = -7;
     light.shadowCameraRight = 7;
     light.shadowCameraTop = 10;
@@ -118,6 +139,9 @@ function createSpotlight() {
     hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
     hemiLight.position.set( 0, 0, 10 );
     scene.add( hemiLight );
+
+    var ambientLight = new THREE.AmbientLight(0xffffff, .10);
+    scene.add(ambientLight);
 
     var hemiHelper = new THREE.HemisphereLightHelper(hemiLight);
     scene.add(hemiHelper);
